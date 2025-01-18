bde651218d05f15c0adb3add353330533e25cf9b
Pyronaitor <jonathancostanzo01@outlook.com>
2025-01-18 09:37:21 -0600

    bismuth block added

--- src/main/java/com/example/examplemod/block/ModBlocks.java
+++ src/main/java/com/example/examplemod/block/ModBlocks.java
@@ -1,15 +1,7 @@
 package com.example.examplemod.block;
 
 import com.example.examplemod.ExampleMod;
-import com.example.examplemod.item.ModItems;
-import com.jcraft.jorbis.Block;
-import net.minecraft.core.Registry;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
@@ -18,19 +10,6 @@ public class ModBlocks {
     public static  final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(ExampleMod.MODID);
 
-    public static final DeferredBlock<Block> BISMUTH_BLOCK = registerBlock("bismuth_block",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
-
-    public static <T extends  Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
-        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name, toReturn);
-        return toReturn;
-    }
-
-    private static<T extends Block> void registerBlockItem(String name, DeferredBlock<T> block){
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
-    }
 
     public static  void register(IEventBus eventBus){
         BLOCKS.register(eventBus);
